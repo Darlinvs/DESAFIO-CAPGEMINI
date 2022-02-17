774dc0a73975a3d6b24b2b486a5f5f33e0a380c6
Darlinvs <vasconcelosdarlane@gmail.com>
2022-02-16 22:23:46 -0300

    Adicionar arquivos de projeto.

---
+++ DESAFIO CAPGEMINI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DESAFIO_CAPGEMINI
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+
+            ///QUESTÃO 1
+        {
+            ///Console.WriteLine("     *\n    **\n   ***\n  ****\n *****\n******");
+
+            /// QUESTÃO 2
+
+            ///   char password;
+            ///  int i = 6;
+
+            ///   Console.WriteLine(" Para criar uma senha forte, é necessário possuir:\nNo mínimo 6 caracteres; /n Conter no mínimo 1 digito;\nConter no mínimo 1 letra em minúsculo;\nConter no mínimo 1 letra em maiúsculo;\nConter no mínimo 1 caractere especial;\nOs caracteres especiais são: !@#$%^&*()-+\nCRIE UMA NOVA SENHA");
+            //  password = Console.ReadLine();
+
+            //  if (password < i)
+            //  {
+            //     Console.WriteLine("Sua senha precisa ter 6 dígitos");
+
+            //  }
+
+            //QUESTÃO 3
+
+           // Desculpa, eu não consegui fazer essa questão.
+
+
+
+
+        }
+
+
+        
+    }
+}
